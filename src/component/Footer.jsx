9007cea17eff8e5f5f2a1d1140e227eaee2f6628
jinyyash <jinyyash@gmail.com>
2022-08-04 15:29:50 +0200

    init

---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h2 className="footer">@Copyright {new Date().getFullYear()}</h2>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/component/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a demo Note</h1>
+      <p>note note</p>
+    </div>
+  );
+}
+
+export default Note;
