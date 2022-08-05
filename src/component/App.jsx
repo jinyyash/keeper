9e8b5e512079e106071f829ad33e8d6fce9d9697
jinyyash <jinyyash@gmail.com>
2022-08-05 13:42:59 +0200

    update for multile notes

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
-      {" "}
       <Header />
-      <Note/>
+      {notes.map((note) => {
+         return <Note key={note.id} title={note.title} content={note.content} />;
+      })}
+
       <Footer />
     </div>
   );
